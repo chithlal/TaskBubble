cca8a867ab2c5b5e6c0b2d600a00091a4f8fd818
Codemac <devcodemac@gmail.com>
2020-08-20 21:40:36 +0530

    Initial commit

---
+++ app/src/main/java/com/developer/chithlal/taskbubble/ui/activities/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.developer.chithlal.taskbubble.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.developer.chithlal.taskbubble.R
+
+class HomeActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
